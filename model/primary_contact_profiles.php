a23816266013f0b0602c7e5bddc23b6abb3739d6
wei jin <wei jin>
2016-11-12 23:47:13 -0800

    2016.11.12 sign in page and primary record page changed

--- model/primary_contact_profiles.php
+++ model/primary_contact_profiles.php
@@ -1,11 +1,11 @@
 <?php
 
 /*  
- * Student Info: Name=Arvin Mai, ID=10010
+ * Student Info: Name=Wei Jin, ID=9983
  * Subject: NPU_2016_Fall_CS556(A)_Team_Project
  * Author: Arvin-tcm 
  * Filename: primary_contact_profiles.php 
- * Date and Time: Nov 10, 2016 11:18:08 PM 
+ * Date and Time: Nov 12, 2016 11:46:08 PM 
  * Project Name: CS556_Team_Project 
  */ 
 
--- model/signin_records.php
+++ model/signin_records.php
@@ -5,7 +5,7 @@
  * Subject: NPU_2016_Fall_CS556(A)_Team_Project
  * Author: Arvin-tcm 
  * Filename: signin_records.php 
- * Date and Time: Nov 10, 2016 11:23:07 PM 
+ * Date and Time: Nov 12, 2016 11:46:07 PM 
  * Project Name: CS556_Team_Project 
  */ 
 class SigninRecords {
